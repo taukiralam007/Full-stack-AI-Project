43de33c31510a1a0274054473686d7ff176da870
taukir.alam007@gmail.com <taukir.alam007@gmail.com>
2025-09-26 20:20:33 +0800

    script.js
    new updates

---
+++ script.js
@@ -0,0 +1,32 @@
+// // keyword ( let , var , const)
+// // variable name (container)
+// //value
+// let age = 30;
+
+// // Declaration first , then assignment
+// let country;
+// country = "India";
+// console.log(country);
+
+// // multiple declaration , then assignments :
+
+// let firstName, lastName;
+// firstName = "Taukir";
+// lastName = "Alam";
+// console.log(firstName, lastName);
+// // Multiple declaration in one line:
+// let age1 = 35,
+//   country1 = "India",
+//   job = "Developer";
+// console.log(age1, country1, job);
+
+//! ------primative type--
+//*. Numbers
+
+let age = 28;
+let price = 19.99;
+console.log(typeof age);
+//* 2. Strings
+let isStudents = false;
+console.log(typeof isStudents);
+console.log(typeof price);
