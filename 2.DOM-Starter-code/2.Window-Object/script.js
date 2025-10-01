f3573754d2a3ccd7ecbed538cc219f81468ac9cc
taukir.alam007@gmail.com <taukir.alam007@gmail.com>
2025-10-01 16:08:21 +0800

    udpated

---
+++ 2.DOM-Starter-code/2.Window-Object/script.js
@@ -0,0 +1,26 @@
+// ---
+// window object
+// console.log(window);
+// console.log(window.document);
+
+// console.log(window.innerHeight);
+console.log(window.innerHeight);
+console.log(innerWidth);
+// console.log(window.innerwidth);
+// console.log(window.innerHeight);
+// console.log(window.location);
+// console.log(window.location.host);
+// console.log(window.location.history);
+// console.log(window.location.host);
+// console.log(window.history);
+// console.log(window.navigator);
+// console.log(window.screen);
+// console.log(window.screen);
+// // console.log(window.alert());
+// console.log(window.confirm());
+// // window.prompt("Enter your name");
+// // window.prompt();
+// window.prompt("Enter your name");
+
+// window.open("https://www.google.com", "_blank", "width=400,height=400");
+console.log(document.body);
